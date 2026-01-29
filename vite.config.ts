db565c45f875c0fc6686cbf506cabd516a3b6670
ravindu4403 <your_github_email@example.com>
2026-01-29 11:33:44 +0530

    Fix: allow Railway host for Vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://jayanthatradersprdbackend-dev.up.railway.app",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+
+  preview: {
+    // Railway domain එක exact එක add කරන්න
+    allowedHosts: ["erp-frontend-production-4f4c.up.railway.app"],
   },
 });
